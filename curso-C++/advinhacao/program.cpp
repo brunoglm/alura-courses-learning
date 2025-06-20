a3e51d0d14266948346ad7b841bfe3c200b10d3b
Bruno Gabriel <bruno.glm_@hotmail.com>
2025-06-20 15:37:17 -0300

    add content

---
+++ curso-C++/advinhacao/program.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "*************************************" << endl;
+  cout << "* Bem vindo ao Jogo de Adivinhação! *" << endl;
+  cout << "*************************************" << endl;
+}
